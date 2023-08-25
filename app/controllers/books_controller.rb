cc38ce12bc7e52e7231c86c67fc38073ba645bed
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-08-25 08:57:05 +0000

    Bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,22 @@
 class BooksController < ApplicationController
+    def index
+        @book = Book.new
+    end
+    
+    def create
+        @book = Book.new(list_params)
+        if @book.save
+            flash[:notice] = "Book was successfully created."
+            redirect_to "/" # 応急的に
+        else 
+            flash.now[:alert] = "Error" # 応急的に
+            render :index
+        end
+    end
+    
+    private
+    # ストロングパラメータ
+    def list_params
+        params.require(:book).permit(:title, :text)
+    end
 end
